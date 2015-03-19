b9d678840b03d51dededaf663a293427d2acec64
Chris Barker <cbarker@puppetlabs.com>
2015-03-19 12:20:30 -0700

    working thanks to finch

--- lib/facter/ec2tags.rb
+++ lib/facter/ec2tags.rb
@@ -1,15 +1,36 @@
-require 'facter'
-require 'aws-sdk'
- 
-if Facter.value("ec2_instance_id") != nil
-   instance_id = Facter.value("ec2_instance_id")
-   region = Facter.value("ec2_placement_availability_zone")[0..-2]
-   ec2 = Aws::EC2::Client.new(region:region)
-   instance = ec2.describe_instances(instance_ids:[instance_id])
-   tags = instance.reservations[0].instances[0].tags
-   tags.each do |tag|
-      fact = "ec2_tag_#{tag["key"]}"
-      Facter.add(fact) { setcode { tag["value"] } }
-   end
-  Facter.add("ec2_region") { setcode { region } }
+Facter.add(:ec2_region)
+  confine do
+    Facter.value(:ec2_instance_id)
+  end
+  setcode do
+    Facter.value("ec2_placement_availability_zone")[0..-2]
+  end
+end
+
+Facter.add(:ec2_tags) do
+  confine do
+    begin
+      require 'aws-sdk-core'
+      true
+    rescue LoadError
+      false
+    end
+  end
+
+  confine do
+    Facter.value(:ec2_instance_id)
+  end
+
+  setcode do
+    instance_id = Facter.value("ec2_instance_id")
+    region = Facter.value("ec2_region")
+    ec2 = Aws::EC2::Client.new(region: region)
+    instance = ec2.describe_instances(instance_ids: [instance_id])
+    tags = instance.reservations[0].instances[0].tags
+    taghash = { }
+    tags.each do |tag|
+      taghash[tag['key']] = tag['value']
+    end
+    taghash
+  end
 end

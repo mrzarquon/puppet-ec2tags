23477daa5e9700919af391e842043c722ce13f41
Chris Barker <chrisb@sneezingdog.com>
2017-11-28 14:52:12 -0800

    Puppet 4 changes
    AIO agent and onward, we just need to put this in the puppet_gem path, and aws-sdk-v3 support means we install the aws-sdk now.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,33 +3,9 @@
 # or you can put `gem install aws-sdk-core` or
 # /opt/puppetlabs/bin/gem install aws-sdk-core in your
 # userdata script
-class ec2tags (
-  $confine_to_aws = True,
-  ){
-
-  if $confine_to_aws == True {
-    if $ec2_metadata != '' {
-      $install_aws_sdk = True
-    }
-    else {
-      $install_aws_sdk = False
-    }
-  }
-  else {
-    $install_aws_sdk = True
-  }
-
-
-
-
-
-  $gem_provider = $is_pe ? {
-    'True'  => 'pe_gem',
-    default => 'gem',
-  }
-
-  package { 'aws-sdk-core':
+class ec2tags {
+  package { 'aws-sdk':
     ensure   => present,
-    provider => $gem_provider,
+    provider => puppet_gem,
   }
 }
